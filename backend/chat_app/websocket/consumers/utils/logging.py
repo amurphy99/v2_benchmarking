5fc35c32b914f1c1a46218c366cfa8fe8d8970e3
amurphy99 <amurphy62299@gmail.com>
2026-02-08 15:14:58 -0600

    Update logging.py

--- backend/chat_app/websocket/consumers/utils/logging.py
+++ backend/chat_app/websocket/consumers/utils/logging.py
@@ -24,19 +24,19 @@ class ChatConsumerLogging:
 
     @staticmethod
     def log_connect_done(user, session_id):
-        logger.info(f"{lu.CC_MAIN} All setup steps for {BOLD}{user}{UNBOLD} succeeded. ChatSession ID: {BOLD}{session_id}{lu.RESET}")
+        logger.info(f"{lu.CC_MAIN} All setup steps for {BOLD}{user}{UNBOLD} succeeded. ChatSession ID: {BOLD}{session_id}{RESET}")
 
     @staticmethod
-    def log_disconnect():
-        pass
+    def log_disconnect(user, code):
+        logger.info(f"{lu.G_LINE_1}{lu.CC_MAIN} {BOLD}{user}{UNBOLD} disconnected (code: {BOLD}{code}{UNBOLD}) {RESET}{lu.G_LINE_2}")
 
 # --------------------------------------------------------------------------------
 # ChatListener Utilities
 # --------------------------------------------------------------------------------
 class ChatListenerLogging:
     @staticmethod
-    def log_connect(username, session_owner):
-        logger.info(f"{lu.Y_LINE_1}{lu.CL_MAIN} {BOLD}{username}{UNBOLD} listening to: {BOLD}{session_owner}{RESET}{lu.Y_LINE_2}")
+    def log_connect(username, session_owner, session_id):
+        logger.info(f"{lu.Y_LINE_1}{lu.CL_MAIN} {BOLD}{username}{UNBOLD} listening to: {BOLD}{session_owner}{UNBOLD}. ChatSession ID: {BOLD}{session_id} {RESET}{lu.Y_LINE_2}")
         
     @staticmethod
     def log_disconnect(username, code):
