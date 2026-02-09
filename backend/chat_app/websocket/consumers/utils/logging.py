f3ecab822a560638892381f1335b5af224e5bafc
amurphy99 <amurphy62299@gmail.com>
2026-02-09 15:49:14 -0600

    Update logging.py

--- backend/chat_app/websocket/consumers/utils/logging.py
+++ backend/chat_app/websocket/consumers/utils/logging.py
@@ -1,7 +1,7 @@
 """
 Logging helpers for the consumer classes 
 --------------------------------------------------------------------------------
-backend.chat_app.websocket.consumers.utils.logging
+`backend.chat_app.websocket.consumers.utils.logging`
 
 Help reduce code bloat in the actual consumer classes.
 
@@ -17,28 +17,32 @@ from ....services.logging_utils import RESET, BOLD, UNBOLD
 # --------------------------------------------------------------------------------
 # ChatConsumer Utilities
 # --------------------------------------------------------------------------------
+from ....services.logging_utils import CC_MAIN, GREEN, G_LINE_1, G_LINE_2
+
 class ChatConsumerLogging:
     @staticmethod
     def log_connect(user, source):
-        logger.info(f"{lu.G_LINE_1}{lu.CC_MAIN} {BOLD}{user}{UNBOLD} opened ChatSession from {BOLD}{source}{RESET}{lu.G_LINE_2}")
+        logger.info(f"{G_LINE_1}{CC_MAIN} {BOLD}{user}{UNBOLD} opened ChatSession from {BOLD}{source}{RESET}{G_LINE_2}")
 
     @staticmethod
     def log_connect_done(user, session_id):
-        logger.info(f"{lu.CC_MAIN} All setup steps for {BOLD}{user}{UNBOLD} succeeded. ChatSession ID: {BOLD}{session_id}{RESET}")
+        logger.info(f"{CC_MAIN} All setup steps for {BOLD}{user}{UNBOLD} succeeded. ChatSession ID: {BOLD}{session_id}{RESET}")
 
     @staticmethod
     def log_disconnect(user, code):
-        logger.info(f"{lu.G_LINE_1}{lu.CC_MAIN} {BOLD}{user}{UNBOLD} disconnected (code: {BOLD}{code}{UNBOLD}) {RESET}{lu.G_LINE_2}")
+        logger.info(f"{G_LINE_1}{CC_MAIN} {BOLD}{user}{UNBOLD} {lu.RED}disconnected{GREEN} (code: {BOLD}{code}{UNBOLD}) {RESET}{G_LINE_2}")
 
 # --------------------------------------------------------------------------------
 # ChatListener Utilities
 # --------------------------------------------------------------------------------
+from ....services.logging_utils import CL_MAIN, Y_LINE_1, Y_LINE_2
+
 class ChatListenerLogging:
     @staticmethod
     def log_connect(username, session_owner, session_id):
-        logger.info(f"{lu.Y_LINE_1}{lu.CL_MAIN} {BOLD}{username}{UNBOLD} listening to: {BOLD}{session_owner}{UNBOLD}. ChatSession ID: {BOLD}{session_id} {RESET}{lu.Y_LINE_2}")
+        logger.info(f"{Y_LINE_1}{CL_MAIN} {BOLD}{username}{UNBOLD} listening to: {BOLD}{session_owner}{UNBOLD}. ChatSession ID: {BOLD}{session_id} {RESET}{Y_LINE_2}")
         
     @staticmethod
     def log_disconnect(username, code):
-        logger.info(f"{lu.Y_LINE_1}{lu.CL_MAIN} {BOLD}{username}{UNBOLD} disconnected (code: {BOLD}{code}{UNBOLD}) {RESET}{lu.Y_LINE_2}")
+        logger.info(f"{Y_LINE_1}{CL_MAIN} {BOLD}{username}{UNBOLD} disconnected (code: {BOLD}{code}{UNBOLD}) {RESET}{Y_LINE_2}")
 
