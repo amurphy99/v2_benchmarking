8530e1c8fa9194ddd36a5dee10643d3c8e515c2e
amurphy99 <amurphy62299@gmail.com>
2026-02-19 12:43:55 -0600

    log unknown json

--- backend/chat_app/websocket/consumers/handlers/ws_events.py
+++ backend/chat_app/websocket/consumers/handlers/ws_events.py
@@ -1,25 +1,24 @@
 """
-Handle events from the WebSocket client.
+Handle events from the ChatConsumer's WebSocket client.
 --------------------------------------------------------------------------------
 `backend.chat_app.websocket.consumers.handlers.ws_events`
 
-These methods get implemented by the consumers.
+These methods get implemented by the consumers via simple passthroughs.
 
 """
 import logging, time
 logger = logging.getLogger(__name__)
 
-
 # From this project
 from ....services import logging_utils as lu 
-from ....services.logging_utils import RESET, BOLD, UNBOLD, CC_MAIN
+from ....services.logging_utils import RESET, BOLD, UNBOLD, CC_MAIN, CC_H, CC_R
 
 # Handling messages
-from  ...services.chatHelpers       import ChatHandler
+from  ...services.chatHelpers import ChatHandler
 
 
 # ================================================================================
-# Handle Incoming Data
+# Handle all forms of incoming data | TODO: are we supposed to guard here more?
 # ================================================================================
 async def handle_receive_json(consumer, data, **kwargs):
     if   data["type"] == "overlapped_speech" : await _handle_overlap(consumer, data=data)
@@ -28,7 +27,13 @@ async def handle_receive_json(consumer, data, **kwargs):
     elif data["type"] == "end_chat"          : consumer.stt_provider.stop(); await consumer.close(code=1000)   
     elif data["type"] == "toggle_stream"     : consumer._toggle_stream(data)
 
-# Overlapped Speech
+    # Unknown JSON
+    else: logger.info(f"{CC_MAIN} {lu.RED}Unknown JSON{CC_R} received: {data} {RESET}")
+
+
+# --------------------------------------------------------------------------------
+# Overlapped Speech | TODO: Not really used for anything at the moment
+# --------------------------------------------------------------------------------
 async def _handle_overlap(consumer, data=None):
     consumer.overlapped_speech_count += 1
     consumer.overlapped_speech_events.append(time.time())
