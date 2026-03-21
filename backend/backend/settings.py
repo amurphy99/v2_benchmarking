ffc5b3c9eeb59ded9c9246db140b005048dd51d7
Abu Adnan Sadi <57232041+Adnan-Sadi@users.noreply.github.com>
2026-03-21 16:56:25 -0500

    hardcoding the host and port in setting for now

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -154,17 +154,21 @@ DATABASES = {
         'NAME'     : config('POSTGRES_DB'),
         'USER'     : config('POSTGRES_USER'),
         'PASSWORD' : config('POSTGRES_PASSWORD'),
+        'HOST'     : "db",
+        'PORT'     : "5432",
         # kept the defaults, so old deployments should still work
-        'HOST'     : config('POSTGRES_HOST', default='db'), 
-        'PORT'     : config('POSTGRES_PORT', default='5432'),
+        #'HOST'     : config('POSTGRES_HOST', default='db'), 
+        #'PORT'     : config('POSTGRES_PORT', default='5432'),
     },
     'vector': {
         'ENGINE'   : 'django.db.backends.postgresql',
         'NAME'     : config('VECTOR_DB_NAME'),
         'USER'     : config('VECTOR_DB_USER'),
         'PASSWORD' : config('VECTOR_DB_PASSWORD'),
-        'HOST'     : config('VECTOR_DB_HOST', default='db_vector'),
-        'PORT'     : config('VECTOR_DB_PORT', default='5432'),
+        'HOST'     : "db_vector",
+        'PORT'     : "5432",
+        #'HOST'     : config('VECTOR_DB_HOST', default='db_vector'),
+        #'PORT'     : config('VECTOR_DB_PORT', default='5432'),
     }
 }
 
