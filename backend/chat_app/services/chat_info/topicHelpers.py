0a755199e0c594108cc6bf07dfbced25e657124d
amurphy99 <amurphy62299@gmail.com>
2026-03-22 11:18:21 -0500

    no short words

--- backend/chat_app/services/chat_info/topicHelpers.py
+++ backend/chat_app/services/chat_info/topicHelpers.py
@@ -1,24 +1,43 @@
+"""
+Get a set of "topics" for a conversation
+--------------------------------------------------------------------------------
+`backend.chat_app.services.chat_info.topicHelpers`
+
+Finds the most common non-stopwords from the user during the chat.
+
+Currently used in offline/local mode when we don't have access to LLM-based
+topic extraction. 
+
+"""
+# NLTK setup
 import nltk
 nltk.download('vader_lexicon')
 nltk.download('stopwords')
 nltk.download('punkt_tab')
 
-from nltk.tokenize        import word_tokenize
-from nltk.corpus          import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.corpus   import stopwords
+from collections   import Counter
 
-from collections import Counter
-    
-def get_topics(message_text): # From freeCodeCamp
-    english_stopwords = stopwords.words("english")
+# Constants
+ENGLISH_STOPWORDS = set(stopwords.words("english"))
+NUM_TOPICS        = 4  # number of topics to get by default
+
+# --------------------------------------------------------------------------------
+# Find N topics from a conversation
+# --------------------------------------------------------------------------------
+def get_topics(message_text: str, n_topics: int = NUM_TOPICS) -> list[str]:
+    """
+    'message_text' => string of concatenated user messages 
+    """
+    # Tokenize the given text
     tokens = word_tokenize(message_text)
-    alpha_lower_tokens = [word.lower() for word in tokens if word.isalpha()]
-    alpha_no_stopwords = [word for word in alpha_lower_tokens if word not in english_stopwords]
-    BoW = Counter(alpha_no_stopwords)
-    most_common = BoW.most_common(4)
-    
-    topics = []
-    for token in most_common:
-        topics.append(token[0])
-        
-    return topics
 
+    # Cast all tokens to lowercase and remove  stopwords
+    alpha_lower_tokens = [word.lower() for word in tokens             if (word.isalpha()) and (len(word) > 3)]
+    alpha_no_stopwords = [word         for word in alpha_lower_tokens if (word not in ENGLISH_STOPWORDS     )]
+
+    # Count the number of times each word appears and take the top N
+    counts = Counter(alpha_no_stopwords)
+    topics = [word[0] for word, _ in counts.most_common(n_topics)]
+    return topics
