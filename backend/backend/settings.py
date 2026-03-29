67defb60a5cb73a9fc9e50eb63cc8f00e423056a
amurphy99 <amurphy62299@gmail.com>
2026-03-29 12:34:09 -0500

    got the media URL to work, audio playback is up
    seems like clicking the word spans isn't working though, doesn't properly seek to to the right spot

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(f"Django settings BASE_DIR: {BASE_DIR}")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
